519e3cc1eed2d241bf481463340289f41d2a4e8d
stefan86g <35009624+stefan86g@users.noreply.github.com>
2018-01-07 20:30:23 +0200

    fix map bag

--- CarRent/MainPageJs.js
+++ CarRent/MainPageJs.js
@@ -11,31 +11,36 @@ function initMap() {
 
     ];
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
-        center: new google.maps.LatLng(32.064030, 34.786602),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+    function func() {
 
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker, i;
-
-    for (i = 0; i < locations.length; i++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-            map: map
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 8,
+            center: new google.maps.LatLng(32.064030, 34.786602),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
         });
 
-        google.maps.event.addListener(marker, 'click', (function (marker, i) {
-            return function () {
-                infowindow.setContent(locations[i][0]);
-                infowindow.open(map, marker);
-            }
-        })(marker, i));
+        var infowindow = new google.maps.InfoWindow();
+
+        var marker, i;
+
+        for (i = 0; i < locations.length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                map: map
+            });
+
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                return function () {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        }
+      
     }
 
-
+    setTimeout(func, 1000);
+  
 }
 
 function GoToPosition(newLat, newLng) {
